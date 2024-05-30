e85ef7c767dd2c700c601ca19eb6c6084c35c1bc
Mohtasim Alam Sohom <94981761+codeblitz97@users.noreply.github.com>
2024-05-30 10:36:24 +0600

    Add site configuration

--- config/site.ts
+++ config/site.ts
@@ -1,69 +1,53 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-  name: "Next.js + NextUI",
-  description: "Make beautiful websites regardless of your design experience.",
+  name: "Kalex",
+  description: "Ad-free anime watching website with the simpliest UI",
   navItems: [
     {
       label: "Home",
       href: "/",
     },
     {
-      label: "Docs",
-      href: "/docs",
+      label: "Trending",
+      href: "/trending",
     },
     {
-      label: "Pricing",
-      href: "/pricing",
-    },
-    {
-      label: "Blog",
-      href: "/blog",
-    },
-    {
-      label: "About",
-      href: "/about",
-    },
-  ],
-  navMenuItems: [
-    {
-      label: "Profile",
-      href: "/profile",
-    },
-    {
-      label: "Dashboard",
-      href: "/dashboard",
-    },
-    {
-      label: "Projects",
-      href: "/projects",
-    },
-    {
-      label: "Team",
-      href: "/team",
-    },
-    {
-      label: "Calendar",
-      href: "/calendar",
+      label: "Popular",
+      href: "/popular",
     },
     {
       label: "Settings",
       href: "/settings",
     },
     {
-      label: "Help & Feedback",
-      href: "/help-feedback",
+      label: "DMCA",
+      href: "/dmca",
+    },
+  ],
+  navMenuItems: [
+    {
+      label: "Home",
+      href: "/home",
     },
     {
-      label: "Logout",
-      href: "/logout",
+      label: "Trending",
+      href: "/trending",
+    },
+    {
+      label: "Popular",
+      href: "/popular",
+    },
+    {
+      label: "Settings",
+      href: "/settings",
+    },
+    {
+      label: "DMCA",
+      href: "/dmca",
     },
   ],
   links: {
-    github: "https://github.com/nextui-org/nextui",
-    twitter: "https://twitter.com/getnextui",
-    docs: "https://nextui.org",
-    discord: "https://discord.gg/9b6yyZKmH4",
-    sponsor: "https://patreon.com/jrgarciadev",
+    github: "https://github.com/codeblitz97/kalex",
   },
 };
