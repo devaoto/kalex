6205770c89cd45a3ec28152315aff7fd0c592502
Mohtasim Alam Sohom <94981761+codeblitz97@users.noreply.github.com>
2024-05-30 10:35:30 +0600

    Add sliding components

--- app/page.tsx
+++ app/page.tsx
@@ -1,56 +1,19 @@
-import { Link } from "@nextui-org/link";
-import { Snippet } from "@nextui-org/snippet";
-import { Code } from "@nextui-org/code";
-import { button as buttonStyles } from "@nextui-org/theme";
+import { use } from "react";
 
-import { siteConfig } from "@/config/site";
-import { title, subtitle } from "@/components/primitives";
-import { GithubIcon } from "@/components/icons";
+import AnimeSlider from "@/components/Slider";
+import { getPopularAnime, getTrendingAnime } from "@/functions/anime";
 
 export default function Home() {
+  const [trending, popular] = use(
+    Promise.all([getTrendingAnime(), getPopularAnime()]),
+  );
+
   return (
-    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-      <div className="inline-block max-w-lg text-center justify-center">
-        <h1 className={title()}>Make&nbsp;</h1>
-        <h1 className={title({ color: "violet" })}>beautiful&nbsp;</h1>
-        <br />
-        <h1 className={title()}>
-          websites regardless of your design experience.
-        </h1>
-        <h2 className={subtitle({ class: "mt-4" })}>
-          Beautiful, fast and modern React UI library.
-        </h2>
-      </div>
-
-      <div className="flex gap-3">
-        <Link
-          isExternal
-          className={buttonStyles({
-            color: "primary",
-            radius: "full",
-            variant: "shadow",
-          })}
-          href={siteConfig.links.docs}
-        >
-          Documentation
-        </Link>
-        <Link
-          isExternal
-          className={buttonStyles({ variant: "bordered", radius: "full" })}
-          href={siteConfig.links.github}
-        >
-          <GithubIcon size={20} />
-          GitHub
-        </Link>
-      </div>
-
-      <div className="mt-8">
-        <Snippet hideCopyButton hideSymbol variant="flat">
-          <span>
-            Get started by editing <Code color="primary">app/page.tsx</Code>
-          </span>
-        </Snippet>
-      </div>
-    </section>
+    <div>
+      <h1 className="text-4xl font-bold">Trending Anime</h1>
+      <AnimeSlider animeData={trending.results} />
+      <h1 className="mt-10 text-4xl font-bold">All Time Popular</h1>
+      <AnimeSlider animeData={popular.results} />
+    </div>
   );
 }
---
+++ components/Slider.tsx
@@ -0,0 +1,82 @@
+"use client";
+
+import useEmblaCarousel from "embla-carousel-react";
+import { Image, Link } from "@nextui-org/react";
+import { EmblaOptionsType } from "embla-carousel";
+
+import {
+  PrevButton,
+  NextButton,
+  usePrevNextButtons,
+} from "./EmblaCarouselArrowButtons";
+
+import { Anime } from "@/types/consumet";
+import "@/styles/slider.css";
+
+export default function AnimeSlider({
+  animeData,
+  options,
+}: {
+  animeData: Anime[];
+  options?: EmblaOptionsType;
+}) {
+  const [emblaRef, emblaApi] = useEmblaCarousel(options);
+
+  const {
+    prevBtnDisabled,
+    nextBtnDisabled,
+    onPrevButtonClick,
+    onNextButtonClick,
+  } = usePrevNextButtons(emblaApi);
+
+  return (
+    <div className="embla relative min-w-full">
+      <div className="">
+        <div className="group">
+          <div className="from-transparent-gr to-transparent-gr group-hover:to-gradient-rgba absolute  z-[69420] flex h-full cursor-pointer items-center bg-gradient-to-r transition-all duration-500 ease-out group-hover:from-[#000000]">
+            <PrevButton
+              disabled={prevBtnDisabled}
+              onClick={onPrevButtonClick}
+            />
+          </div>
+          <div className="from-transparent-gr to-transparent-gr group-hover:to-gradient-rgba absolute right-0 z-[69420] flex h-full cursor-pointer items-center bg-gradient-to-l transition-all duration-500 ease-out group-hover:from-[#000000]">
+            <NextButton
+              disabled={nextBtnDisabled}
+              onClick={onNextButtonClick}
+            />
+          </div>
+        </div>
+      </div>
+      <div ref={emblaRef as any} className="embla__viewport">
+        <div className="embla__container">
+          {animeData.map((anime) => (
+            <div key={anime.id} className="embla__slide">
+              <Link
+                className="flex flex-col items-start text-foreground"
+                href={`/info/${anime.id}?releasing=${anime.status === "Ongoing"}`}
+              >
+                <Image
+                  alt={anime.title.english ?? anime.title.romaji}
+                  className="max-h-[360px] min-h-[360px] min-w-[255px] max-w-[255px] object-cover"
+                  src={anime.image}
+                />
+                <h1 className="mt-1 line-clamp-1 font-semibold">
+                  {anime.title.english ??
+                    anime.title.userPreferred ??
+                    anime.title.romaji}
+                </h1>
+                <div className="flex items-center gap-2 font-medium">
+                  <p>{anime.status}</p>
+                  <div className="size-[6px] rounded-full bg-foreground" />
+                  <p>{anime.totalEpisodes} Episodes</p>
+                  <div className="size-[6px] rounded-full bg-foreground" />
+                  <p>{anime.type}</p>
+                </div>
+              </Link>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
